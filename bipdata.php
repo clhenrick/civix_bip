0acde04e17489303e3e64080cfe19972036fbf21
Sophia Russell <sophia.elizabeth.russell@gmail.com>
2016-05-08 14:42:37 -0400

    pre hook

--- bipdata.php
+++ bipdata.php
@@ -71,9 +71,111 @@ function bipdata_civicrm_pre($op, $objectName, $id, &$params) {
       // Write some code to get the BIP values from someplace else.  Could be an API call or a SQL call or whatever.
       // Example using API (assumes BIP data is its own object)
       $result = civicrm_api3('BipData', 'get', array(
-        'bip_bbl' => $params[$custom_field]
+        'bip_bbl' => $params['custom_153']
       ));
-      echo($result);
+      $params['bip_address_270'] == $result['address'];
+      $params['res_unit_154'] == $result['res_unit'];
+      $params['tot_unit_155'] == $result['tot_unit'];
+      $params['num_floors_156'] == $result['num_floors'];
+      $params['census_tract_157'] == $result['census_tract'];
+      $params['borocode_159'] == $result['borocode'];
+      $params['block_160'] == $result['block'];
+      $params['lot_161'] == $result['lot'];
+      $params['commdist_162'] == $result['commdist'];
+      $params['lucategory_163'] == $result['lucategory'];
+      $params['year_built_164'] == $result['year_built'];
+      $params['yr_1st_alt_165'] == $result['yr_1st_alt'];
+      $params['yr_2nd_alt_166'] == $result['yr_2nd_alt'];
+      $params['bldg_class_167'] == $result['bldg_class'];
+      $params['zoning_gen_168'] == $result['zoning_gen'];
+      $params['zoning_169'] == $result['zoning'];
+      $params['open_violations_171'] == $result['open_violations'];
+      $params['open_a_violations_172'] == $result['open_a_violations'];
+      $params['open_b_violations_173'] == $result['open_b_violations'];
+      $params['open_c_violations_174'] == $result['open_c_violations'];
+      $params['prioryear_violations_175'] == $result['prioryear_violations'];
+      $params['prioryear_a_violations_176'] == $result['prioryear_a_violations'];
+      $params['prioryear_b_violations_177'] == $result['prioryear_b_violations'];
+      $params['prioryear_c_violations_178'] == $result['prioryear_c_violations'];
+      $params['seven_a_179'] == $result['seven_a'];
+      $params['aep_180'] == $result['aep'];
+      $params['ppi_181'] == $result['ppi'];
+      $params['underlying_conditions_182'] == $result['underlying_conditions'];
+      $params['nyc_421a_exempt_properties_183'] == $result['nyc_421a_exempt_properties'];
+      $params['dob_violation_186'] == $result['dob_violation'];
+      $params['ecb_violation_187'] == $result['ecb_violation'];
+      $params['city_lien_188'] == $result['city_lien'];
+
+      $params['water_190'] == $result['water'];
+      $params['umbrella_192'] == $result['umbrella'];
+      $params['party_name_193'] == $result['party_name'];
+      $params['document_type_194'] == $result['document_type'];
+      $params['recorded_filed_196'] == $result['recorded_filed'];
+      $params['absolute_197'] == $result['absolute'];
+      $params['per_unit_198'] == $result['per_unit'];
+      $params['current_bip_score_199'] == $result['current_bip_score'];
+      $params['score_2015april_200'] == $result['score_2015april'];
+      $params['score_2015jan_201'] == $result['score_2015jan'];
+      $params['score_2014oct_202'] == $result['score_2014oct'];
+      $params['score_2014jun_203'] == $result['score_2014jun'];
+      $params['score_2014feb_204'] == $result['score_2014feb'];
+      $params['score_2013nov_205'] == $result['score_2013nov'];
+      $params['score_2013aug_206'] == $result['score_2013aug'];
+      $params['score_2013april_207'] == $result['score_2013april'];
+      $params['score_2012nov_208'] == $result['score_2012nov'];
+      $params['score_2012june_209'] == $params['score_2012june'];
+      $params['score_2012jan_210'] == $params['score_2012jan'];
+      $params['score_2010_2_211'] == $params['score_2010_2'];
+      $params['score_2010_1_212'] == $params['score_2010_1'];
+      $params['score_2009_2_213'] == $params['score_2009_2'];
+      $params['score_2009_1_214'] == $params['score_2009_1'];
+      $params['score_2008_2_215'] == $params['score_2008_2'];
+      $params['occurance_216'] == $result['occurance'];
+      $params['average_217'] == $result['average'];
+      $params['cdfavg_218'] == $result['cdfavg'];
+      $params['corpname_219'] == $result['corpname'];
+      $params['corpstreet_221'] == $result['corpstreet'];
+      $params['corpapt_222'] == $result['corpapt'];
+      $params['corpcity_223'] == $result['corpcity'];
+      $params['corpstate_224'] == $result['corpstate'];
+
+      $params['indivfirstname_226'] == $result['indivfirstname'];
+      $params['indivlastname_227'] == $result['indivlastname'];
+
+      $params['indivstreet_229'] == $result['indivstreet'];
+      $params['indivapt_230'] == $result['indivapt'];
+      $params['indivcity_231'] == $result['indivcity'];
+      $params['indivst_232'] == $result['indivst'];
+      $params['headofftitle_234'] == $result['headofftitle'];
+      $params['headofffirstname_235'] == $result['headofffirstname'];
+      $params['headofflastname_236'] == $result['headofflastname'];
+      $params['headoffstreet_238'] == $result['headoffstreet'];
+      $params['headoffcity_240'] == $result['headoffcity'];
+      $params['headoffst_241'] == $result['headoffst'];
+      $params['mngmtcorp_243'] == $result['mngmtcorp'];
+      $params['mngmtfirstname_244'] == $result['mngmtfirstname'];
+      $params['mngmtlastname_245'] == $result['mngmtlastname'];
+      $params['mngmtstreetnum_246'] == $result['mngmtstreetnum'];
+      $params['mngmtstreet_247'] == $result['mngmtstreet'];
+      $params['mngmtapt_248'] == $result['mngmtapt'];
+      $params['mngmtcity_249'] == $result['mngmtcity'];
+      $params['mngmtst_250'] == $result['mngmtst'];
+      $params['violations_current_as_of_252'] == $result['violations_current_as_of'];
+      $params['headoffapt_255'] == $result['headoffapt'];
+      $params['corpstreetnum_256'] == $result['corpstreetnum'];
+      $params['headoffstreetnum_257'] == $result['headoffstreetnum'];
+      $params['erps_258'] == $result['erps'];
+      $params['soldliensopen_260'] == $result['soldliensopen'];
+      $params['open_i_violations_261'] == $result['open_i_violations'];
+      $params['prioryear_v_to_c_ratio_262'] == $result['prioryear_v_to_c_ratio'];
+      $params['indivstreetnum_263'] == $result['indivstreetnum'];
+      $params['headoffzip_264'] == $result['headoffzip'];
+      $params['indivzip_265'] == $result['indivzip'];
+      $params['doc_amount_266'] == $result['doc_amount'];
+
+      $params['corpzip_267'] == $result['corpzip'];
+      $params['mngmtzip_268'] == $result['mngmtzip'];
+      $params['zip_code_269'] == $result['zip_code'];
     }
   }
 }
